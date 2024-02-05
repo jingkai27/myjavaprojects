67ec04721cfe86a647b55b5b1cd24dccd79567a4
jingkai27 <jingkai.t27@gmail.com>
2024-02-05 14:58:35 +0800

    updated

--- sutd_psets/Problem-Set-1A/HW2.3/CheckingAccount.java
+++ sutd_psets/Problem-Set-1A/HW2.3/CheckingAccount.java
@@ -4,7 +4,27 @@
 // Leave the Account.class file alone  
 // If your class has problems accessing the Account.class, 
 // go to Actions -> Reset Assignment. Make sure you have your code stored on your computer. 
+/* In Week-1, the Account class was defined to model a bank account.
+An account has the properties: account id, balance, annual interest rate, and date created, and
+methods to deposit and withdraw funds.
+
+OK - Create a subclass for checking account CheckingAccount.
+A checking account has an overdraft limit of 5000. Provide constructors for CheckingAccount similar
+to Account. Override withdraw() to print out “over limit” if
+the amount withdrawing exceeds the overdraft limit. */
 
 public class CheckingAccount extends Account{
 
+    CheckingAccount(int id, double balance){
+        super(id, balance);
+    }
+
+    public void withdraw(double amount) {
+        if ((this.getBalance() - amount) < -5000){
+            System.out.println("Over limit");
+        } else {
+            double currBalance = this.getBalance();
+            this.setBalance(currBalance - amount);
+        }
+    }
 }
