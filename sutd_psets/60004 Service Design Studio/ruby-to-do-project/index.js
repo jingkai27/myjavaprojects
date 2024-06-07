bd374d7463f6cd04421b8274c26268f957cd3a64
jingkai27 <jingkai.t27@gmail.com>
2024-06-07 23:54:22 +0800

    Implementing node.js features

--- sutd_psets/60004 Service Design Studio/ruby-to-do-project/index.js
+++ sutd_psets/60004 Service Design Studio/ruby-to-do-project/index.js
@@ -2,11 +2,28 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
+const TODO_FILE = 'todos.json';
 
 app.use(bodyParser.json());
+
+// Helper function to read todos from the file
+const readTodosFromFile = () => {
+    try {
+        const data = fs.readFileSync(TODO_FILE, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+};
+
+// Helper function to write todos to the file
+const writeTodosToFile = (todos) => {
+    fs.writeFileSync(TODO_FILE, JSON.stringify(todos, null, 2));
+};
 
 // Get all to-dos
 app.get('/todos', (req, res) => {
@@ -24,14 +41,18 @@ app.post('/todos', (req, res) => {
 app.put('/todos/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
     const updatedTodo = req.body;
+    let todos = readTodosFromFile();
     todos = todos.map(todo => todo.id === id ? updatedTodo : todo);
+    writeTodosToFile(todos);
     res.json(updatedTodo);
 });
 
 // Delete a to-do
 app.delete('/todos/:id', (req, res) => {
     const id = parseInt(req.params.id, 10);
+    let todos = readTodosFromFile();
     todos = todos.filter(todo => todo.id !== id);
+    writeTodosToFile(todos);
     res.status(204).send();
 });
 
